c76641451077ab92a2e5b1b53bb7dc9ecb150c5f
Shruti27272727 <Shruti.powar2727@gmail.com>
2025-10-06 10:00:39 +0530

    Test4

--- citizen-portal/vite.config.js
+++ citizen-portal/vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     host: true,
-    open: true
+    open: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'scintillating-beauty-production-1e08.up.railway.app' // ✅ your Railway domain
+    ],
   },
   plugins: [react()],
 });
